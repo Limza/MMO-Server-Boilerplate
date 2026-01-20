9f473ddcde582e45a4ec716f66af0ce976e61fde
jun <rkddlrwns@naver.com>
2026-01-20 18:42:12 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MMO_Server_Boilerplate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Thread.Sleep(10000);
+    }
+}
